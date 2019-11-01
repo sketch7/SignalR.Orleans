29a7ebf822d62e9846ceb5cf97604b0e54aff72b
Stephen Lautier <stephen.lautier@outlook.com>
2019-11-01 16:57:43 +0100

    fix internal

--- src/SignalR.Orleans/Properties/AssemblyInfo.cs
+++ src/SignalR.Orleans/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 
 
 [assembly: InternalsVisibleTo("SignalR.Orleans.Tests")]
+[assembly: InternalsVisibleTo("SignalR.Orleans.AspNet")]
 public class AssemblyInfo
 {
 }
